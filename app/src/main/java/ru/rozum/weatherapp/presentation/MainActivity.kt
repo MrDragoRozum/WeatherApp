226df61229c7d8fb852beefbe0b25525b8953be6
Alexander Rozum <roozumaleks@gmail.com>
2024-07-04 23:22:32 +0300

    Реализация FavouriteContent часть 1

--- app/src/main/java/ru/rozum/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/rozum/weatherapp/presentation/MainActivity.kt
@@ -3,24 +3,14 @@ package ru.rozum.weatherapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.arkivanov.decompose.defaultComponentContext
 import kotlinx.coroutines.launch
 import ru.rozum.weatherapp.WeatherApp
-import ru.rozum.weatherapp.data.network.api.ApiFactory
-import ru.rozum.weatherapp.domain.entity.Weather
+import ru.rozum.weatherapp.domain.usecase.ChangeFavouriteCitiesUseCase
+import ru.rozum.weatherapp.domain.usecase.SearchCityUseCase
 import ru.rozum.weatherapp.presentation.root.DefaultRootComponent
-import ru.rozum.weatherapp.presentation.root.RootComponent
 import ru.rozum.weatherapp.presentation.root.RootContent
-import ru.rozum.weatherapp.presentation.theme.WeatherAppTheme
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +23,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val root = rootComponentFactory.create(defaultComponentContext())
-        
+
+
         setContent {
             RootContent(component = root)
         }
